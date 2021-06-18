55b40b97f2fb71a4d4382dc1faf0daf279c3da4d
kaushikmedhi <kaushikmedhi97@gmail.com>
2021-06-19 01:28:59 +0530

    Added photo saving feature to addbook1.php

--- library/admin/addbook1.php
+++ library/admin/addbook1.php
@@ -1,36 +1,56 @@
 
 <?php
 
-
-// $con=mysqli_connect("localhost","root1","pass","library")or die("can't connect...");
-
 include '../connect.php';
 
+$b_id = $_POST["b_id"];
+$b_name = $_POST["b_name"];
+$b_description = $_POST["b_description"];
+$quantity = $_POST["quantity"];
+$author = $_POST["author"];
+$year = $_POST["year"];
+$category = $_POST["category"];
+$isbn = $_POST["isbn"];
+$language = $_POST["language"];
+$file = $_FILES["file"];
+print_r($file);
 
-if($_SERVER['REQUEST_METHOD'] == 'POST') 
-{
-    $b_id = $_POST["b_id"];
-    $b_name = $_POST["b_name"];
-    $b_description = $_POST["b_description"];
-    $quantity = $_POST["quantity"];
-	$author = $_POST["author"];
-	$year = $_POST["year"];
-	$category = $_POST["category"];
-	$isbn = $_POST["isbn"];
-	$language = $_POST["language"];
-	$photo = $_POST["photo"];
+if (isset($_POST['submit'])) {
 
-    $query = "INSERT INTO books VALUES ('$b_id', '$b_name', '$b_description', '$quantity', '$author', '$year', '$category', '$isbn', '$photo', '$language') ";
-    
-    if(mysqli_query($con,$query))
-	{
-		header("location:adminpanel.php?ok=1");
-	}
-	else
-	{
-		echo mysqli_error($con);
+	$filename = $file['name'];
+	$filerror = $file['error'];
+	$filetemp = $file['tmp_name'];
+	$filetype = $file['type'];
+	$filesize = $file['size'];
+
+	$fileExt = explode('.', $filename);
+	$fileActualEXt = strtolower(end($fileExt));
+
+	$allowed = array('jpg', 'jpeg', 'png');
+
+	if (in_array($fileActualEXt, $allowed)) {
+		if ($filerror === 0) {
+			if ($filesize < 5120000) {
+				$filenamenew = uniqid('', true) . "." . $fileActualEXt;
+				$filedestination = 'uploads/' . $filenamenew;
+				move_uploaded_file($filetemp, $filedestination);
+
+				$query = "INSERT INTO books VALUES ('$b_id', '$b_name', '$b_description', '$quantity', '$author', '$year', '$category', '$isbn', '$filedestination', '$language') ";
+
+				if (mysqli_query($con, $query)) {
+					header("location:viewbook.php");
+				} else {
+					echo mysqli_error($con);
+				}
+			} else {
+				echo "File size should be less than 5mb";
+			}
+		} else {
+			echo "There was an error uploading your file";
+		}
+	} else {
+		echo " Invalid File type";
 	}
 }
-
 ?>
 
--- library/admin/viewbook.php
+++ library/admin/viewbook.php
@@ -1,21 +1,15 @@
 <?php
 session_start();
 
-
-
 include '../connect.php';
-// $con=mysqli_connect("localhost","root1","pass","library")or die("can't connect...");
-
-
-
-
 ?>
 
 <html>
 
 
 
-    <title>library</title>
+<title>library</title>
+
 <head>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <!-- Required meta tags -->
@@ -23,8 +17,7 @@ include '../connect.php';
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="//cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
 
 
@@ -32,78 +25,75 @@ include '../connect.php';
 </head>
 
 <body>
-<div class="container my-4">
+    <div class="container my-4">
 
-    <h1 style="text-align: center;">View Book</h1>
-</div>
+        <h1 style="text-align: center;">View Book</h1>
+    </div>
 
-  
-<div class="container my-4">
-    <table class="table" id="myTable" >
-        <thead>
-            <tr>
-                <td>Photo</td>
-                <td>ID</td>
-                <td>Name</td>
-                <td>Description</td>
-                <td>quantity</td>
-                <td>Author</td>
-                <td>Year of publication</td>
-                <td>Category</td>
-                <td>ISBN</td>
-                <td>Language</td>
-                <td>Action</td>
-            </tr>
-        </thead>
 
-        <tbody>
-         <?php
-
-        $result = mysqli_query($con, "select * from books");
-        while ($row = mysqli_fetch_array($result)){
-            echo '
+    <div class="container my-4">
+        <table class="table" id="myTable">
+            <thead>
                 <tr>
-                    <td><img src="'.$row['photo'].'" width="80" height="120"></td>
-                    <td>'.$row["b_id"].'</td>
-                    <td>'.$row["b_name"].'</td>
-                    <td>'.$row["b_description"].'</td>
-                    <td>'.$row["quantity"].'</td>
-                    <td>'.$row["author"].'</td>
-                    <td>'.$row["year"].'</td>
-                    <td>'.$row["category"].'</td>
-                    <td>'.$row["isbn"].'</td>
-                    <td>'.$row["language"].'</td>
-                    <td><button><a href="editbook.php?b_id='.$row["b_id"].'">EDIT</a></button>
-                    <button><a href="deletebook.php?b_id='.$row["b_id"].'">DELETE</a></button></td>
+                    <td>Photo</td>
+                    <td>ID</td>
+                    <td>Name</td>
+                    <td>Description</td>
+                    <td>quantity</td>
+                    <td>Author</td>
+                    <td>Year of publication</td>
+                    <td>Category</td>
+                    <td>ISBN</td>
+                    <td>Language</td>
+                    <td>Action</td>
+                </tr>
+            </thead>
+
+            <tbody>
+                <?php
+
+                $result = mysqli_query($con, "select * from books");
+                while ($row = mysqli_fetch_array($result)) {
+                    echo '
+                <tr>
+                    <td><img src="' . $row['photo'] . '" width="80" height="120"></td>
+                    <td>' . $row["b_id"] . '</td>
+                    <td>' . $row["b_name"] . '</td>
+                    <td>' . $row["b_description"] . '</td>
+                    <td>' . $row["quantity"] . '</td>
+                    <td>' . $row["author"] . '</td>
+                    <td>' . $row["year"] . '</td>
+                    <td>' . $row["category"] . '</td>
+                    <td>' . $row["isbn"] . '</td>
+                    <td>' . $row["language"] . '</td>
+                    <td><button><a href="editbook.php?b_id=' . $row["b_id"] . '">EDIT</a></button>
+                    <button><a href="deletebook.php?b_id=' . $row["b_id"] . '">DELETE</a></button></td>
             ';
-        } 
-        ?>
-        </tbody>
+                }
+                ?>
+            </tbody>
 
 
-    </table>
+        </table>
     </div>
 
     <div>
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-        <script src="https://code.jquery.com/jquery-3.6.0.js"
-            integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-            integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
         </script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-            integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
         </script>
         <script src="//cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
         <script>
-        $(document).ready(function() {
-            $('#myTable').DataTable({
-                responsive: true
-            });
+            $(document).ready(function() {
+                $('#myTable').DataTable({
+                    responsive: true
+                });
 
-        });
+            });
         </script>
     </div>
 
