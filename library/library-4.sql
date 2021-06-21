8f619ed44066885d466b47b92efbe94267822e1a
Ankur <41209837+djankur@users.noreply.github.com>
2021-06-21 14:43:40 +0530

    library-4.sql

--- library/library-4.sql
+++ library/library-4.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.7
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:8889
--- Generation Time: Jun 14, 2021 at 02:05 PM
--- Server version: 5.7.32
--- PHP Version: 7.4.12
+-- Host: 127.0.0.1
+-- Generation Time: Jun 21, 2021 at 11:11 AM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 7.3.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -65,7 +66,9 @@ CREATE TABLE `books` (
 --
 
 INSERT INTO `books` (`b_id`, `b_name`, `b_description`, `quantity`, `author`, `year`, `category`, `isbn`, `photo`, `language`) VALUES
-(101, 'book name', 'book details', 3, 'author name', 2021, 'computer science', 'isbn112283961', 'photo.jpg', 'english');
+(1, 'English', 'e', 5, 'ad', 2111, 'ca', 'a123d', 'uploads/60cf32db6a1cc3.93861990.jpg', 'eng'),
+(101, 'Let us C', 'C language', 5, 'Dennies richie', 2006, 'MCA,CSE', '10000', '', 'axa'),
+(1001, 'sw eng', 'se', 5, 'ajd', 2011, 'mca,cse', '789456123', 'uploads/60cf8480ebc770.57314533.jpg', 's');
 
 -- --------------------------------------------------------
 
@@ -74,12 +77,19 @@ INSERT INTO `books` (`b_id`, `b_name`, `b_description`, `quantity`, `author`, `y
 --
 
 CREATE TABLE `books_issue` (
+  `id` int(11) NOT NULL,
   `b_id` int(255) NOT NULL,
   `s_id` int(255) NOT NULL,
   `issue_quantity` int(255) NOT NULL,
-  `issue_date` date NOT NULL,
-  `issue_time` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6)
+  `issue_date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `books_issue`
+--
+
+INSERT INTO `books_issue` (`id`, `b_id`, `s_id`, `issue_quantity`, `issue_date`) VALUES
+(1, 1, 2, 1, '2021-06-20 18:30:00');
 
 -- --------------------------------------------------------
 
@@ -90,11 +100,24 @@ CREATE TABLE `books_issue` (
 CREATE TABLE `student` (
   `s_id` int(255) NOT NULL,
   `s_name` varchar(2552) NOT NULL,
+  `s_pasword` varchar(255) NOT NULL,
   `s_email` varchar(255) NOT NULL,
-  `s_phone` int(255) NOT NULL,
+  `s_phone` bigint(255) NOT NULL,
   `s_address` varchar(255) NOT NULL,
-  `department` varchar(255) NOT NULL
+  `department` varchar(255) NOT NULL,
+  `reg_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `update_date` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
+  `status` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`s_id`, `s_name`, `s_pasword`, `s_email`, `s_phone`, `s_address`, `department`, `reg_date`, `update_date`, `status`) VALUES
+(2, 'Jugantar', 'password123', 'jug@jug.jug', 9909876567, 'earth', 'computer science', '2021-06-19 08:14:22', NULL, 0),
+(3, 'meghnath', 'abcd123', 'abc@ds.af', 8898766765, 'mars', 'physics', '2021-06-19 08:36:14', NULL, 0),
+(7, 'Ankur', 'ajd', 'a@a.com', 70025445565, 'gol', 'MCA', '2021-06-20 18:09:08', NULL, 0);
 
 --
 -- Indexes for dumped tables
@@ -116,6 +139,7 @@ ALTER TABLE `books`
 -- Indexes for table `books_issue`
 --
 ALTER TABLE `books_issue`
+  ADD PRIMARY KEY (`id`),
   ADD KEY `b_id` (`b_id`),
   ADD KEY `s_id` (`s_id`);
 
@@ -136,10 +160,16 @@ ALTER TABLE `admin`
   MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT for table `books_issue`
+--
+ALTER TABLE `books_issue`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
+
+--
 -- AUTO_INCREMENT for table `student`
 --
 ALTER TABLE `student`
-  MODIFY `s_id` int(255) NOT NULL AUTO_INCREMENT;
+  MODIFY `s_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Constraints for dumped tables
@@ -149,8 +179,9 @@ ALTER TABLE `student`
 -- Constraints for table `books_issue`
 --
 ALTER TABLE `books_issue`
-  ADD CONSTRAINT `books_issue_ibfk_1` FOREIGN KEY (`b_id`) REFERENCES `books` (`b_id`),
-  ADD CONSTRAINT `books_issue_ibfk_2` FOREIGN KEY (`s_id`) REFERENCES `student` (`s_id`);
+  ADD CONSTRAINT `books_issue_ibfk_1` FOREIGN KEY (`b_id`) REFERENCES `books` (`b_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `books_issue_ibfk_2` FOREIGN KEY (`s_id`) REFERENCES `student` (`s_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
