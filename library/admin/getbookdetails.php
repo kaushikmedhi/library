4e26ebbddf0438111a7595c67d3501d9e82f2702
kaushikmedhi <kaushikmedhi97@gmail.com>
2021-07-08 13:44:37 +0530

    updated database and updated issue book

--- library/admin/getbookdetails.php
+++ library/admin/getbookdetails.php
@@ -1,33 +1,37 @@
 <?php
-  
+
 // Get the b_id 
 $b_id = $_REQUEST['b_id'];
-  
+
 include '../connect.php';
-  
+
 if ($b_id !== "") {
-      
+
     // Get corresponding first name and 
     // isbn for that b_id    
-    $query = mysqli_query($con, "SELECT isbn FROM book_status WHERE b_id='$b_id'");
-    
+    if (strlen((string)$b_id) <> 6) {
+        $result = array(NULL, NULL, NULL);
+    } else {
+        $query = mysqli_query($con, "SELECT * FROM book_status WHERE b_id='$b_id'");
 
-    $row = mysqli_fetch_array($query);
-    
-    // Get the isbn
-    $isbn = $row["isbn"];
-    
 
-    $query2 = mysqli_query($con, "SELECT * FROM books WHERE isbn='$isbn'");
-    $row2 = mysqli_fetch_array($query2);
-    $name = $row2["b_name"];
-    $category = $row2["category"];
+        $row = mysqli_fetch_array($query);
+
+        // Get the isbn
+        $isbn = $row["isbn"];
+        $status = $row["status"];
+
+
+        $query2 = mysqli_query($con, "SELECT b_name FROM books WHERE isbn='$isbn'");
+        $row2 = mysqli_fetch_array($query2);
+        $name = $row2["b_name"];
+        $result = array("$isbn", "$name", "$status");
+    }
 }
-  
+
 // Store it in a array
-$result = array("$isbn","$name","$category");
-  
+
+
 // Send in JSON encoded form
 $myJSON = json_encode($result);
 echo $myJSON;
-?>
--- library/admin/issuebook.php
+++ library/admin/issuebook.php
@@ -23,11 +23,11 @@ include 'main.php';
 
 <body>
 
-<style>
-.col{
-  margin: 20px;
-}
-</style>
+  <style>
+    .col {
+      margin: 20px;
+    }
+  </style>
 
   <script src="https://code.jquery.com/jquery-3.2.1.min.js">
   </script>
@@ -37,76 +37,87 @@ include 'main.php';
 
 
   <form class="form-issuebook" action="issuebook1.php" method="POST">
-  
-   <div class="row">
 
-    <div class="col">
-      <input type="text" name="b_id" class="form-control" onkeyup="GetDetail(this.value)" placeholder="Enter the Book ID">
-      
-    </div>
+    <div class="row">
 
-    <div class="col">
-      <input type="text" name="s_id" class="form-control" onkeyup="GetStudent(this.value)" placeholder="Enter the Student ID">
-      
-    </div>
+      <div class="col">
+        <input type="text" name="b_id" class="form-control" onkeyup="GetDetail(this.value)" placeholder="Enter the Book ID">
 
-   </div>
-
-   <div class="row">
-
-    <div class="col">
-    ISBN:
-      <input type="text" name="isbn" id="isbn" class="form-control-plaintext readonly" placeholder='Invalid book id' value="" required>
       </div>
 
       <div class="col">
-    Student Name:
-    <input type="text" name="s_name" id="s_name" class="form-control-plaintext readonly" placeholder='Invalid student id' value="" required>
+        <input type="text" name="s_id" class="form-control" onkeyup="GetStudent(this.value)" placeholder="Enter the Student ID">
+
+      </div>
+
     </div>
 
-   </div>
-   
-   <div class="row">
-    <div class="col">
-    Book Name:
-      <input type="text" name="name" id="name" class="form-control-plaintext readonly" placeholder='Invalid book id' value="" required>
+    <div class="row">
+
+      <div class="col">
+        ISBN:
+        <input type="text" name="isbn" id="isbn" class="form-control-plaintext readonly" placeholder='Invalid book id' value="" required>
+      </div>
+
+      <div class="col">
+        Student Name:
+        <input type="text" name="s_name" id="s_name" class="form-control-plaintext readonly" placeholder='Invalid student id' value="" required>
+      </div>
+
     </div>
 
-    <div class="col">
-    Department:
-      <input type="text" name="department" id="department" class="form-control-plaintext readonly" placeholder='Invalid student id' value="" required>
-    </div>
-   </div>
+    <div class="row">
+      <div class="col">
+        Book Name:
+        <input type="text" name="name" id="name" class="form-control-plaintext readonly" placeholder='Invalid book id' value="" required>
+      </div>
 
-<div class="row">
-   <div class="col">
-    Category:
-      <input type="text" name="category" id="category" class="form-control-plaintext readonly" placeholder='Invalid book id' value="" required>
+      <div class="col">
+        Department:
+        <input type="text" name="department" id="department" class="form-control-plaintext readonly" placeholder='Invalid student id' value="" required>
+      </div>
     </div>
-</div>
-    
 
-    <input class="btn btn-md btn-primary btn-block text-uppercase" name="submit" type="submit"><br><br>
+    <div class="row">
+      <div class="col">
+        Status:
+        <input type="text" name="status" id="status" class="form-control-plaintext readonly" placeholder='Invalid book id' value="" required>
+      </div>
+      <div class="col">
+        Total books issued by the student:
+        <input type="text" name="issued_books" id="issued_books" class="form-control-plaintext readonly" placeholder='Invalid ' value="" required>
+      </div>
+    </div>
+
+
+    <input class="btn btn-md btn-primary btn-block text-uppercase" name="submit" id="submit" type="submit" disabled=true><br><br>
 
   </form>
 
   <script>
-  // readonly fields
-    $(".readonly").on('keydown paste focus mousedown', function(e){
-        if(e.keyCode != 9) // ignore tab
-            e.preventDefault();
+    // readonly fields
+    $(".readonly").on('keydown paste focus mousedown', function(e) {
+      if (e.keyCode != 9) // ignore tab
+        e.preventDefault();
     });
-</script>
+  </script>
 
   <script>
     // onkeyup event will occur when the user 
     // release the key and calls the function
     // assigned to this event
+    let disableBook = true;
+    let disableStudent = true;
+
     function GetDetail(str) {
+      document.getElementById("submit").disabled = true;
+      document.getElementById("status").classList.remove("text-white", "bg-success", "bg-danger", "fw-bolder");
+      document.getElementById("status").value = "";
+
       if (str.length == 0) {
         document.getElementById("isbn").value = "";
         document.getElementById("name").value = "";
-        document.getElementById("category").value = "";
+        document.getElementById("status").value = "";
         return;
       } else {
 
@@ -132,8 +143,23 @@ include 'main.php';
 
             // Assign the value received to
             // other input field
-            document.getElementById("name").value = myObj[1];
-            document.getElementById("category").value = myObj[2];
+            document.getElementById("name").value = myObj[1];;
+            if (myObj[2] == 1) {
+              disableBook = true;
+              document.getElementById("status").value = "Acquired";
+              document.getElementById("status").classList.add("text-white", "bg-danger", "fw-bolder");
+              document.getElementById("status").classList.remove("bg-success");
+
+            } else if (myObj[2] == 0 && myObj[2] != "") {
+              disableBook = false;
+              console.log(myObj);
+              document.getElementById("status").value = "Available";
+              document.getElementById("status").classList.add("text-white", "bg-success", "fw-bolder");
+              document.getElementById("status").classList.remove("bg-danger");
+            }
+            if (!disableBook && !disableStudent) {
+              document.getElementById("submit").disabled = false;
+            }
           }
         };
 
@@ -146,9 +172,15 @@ include 'main.php';
     }
 
     function GetStudent(str) {
+      document.getElementById("submit").disabled = true;
+      document.getElementById("issued_books").classList.remove("text-white", "bg-success", "bg-danger", "fw-bolder");
+
       if (str.length == 0) {
         document.getElementById("s_name").value = "";
         document.getElementById("department").value = "";
+        document.getElementById("issued_books").value = "";
+
+
         return;
       } else {
         var xmlhttp = new XMLHttpRequest();
@@ -162,7 +194,25 @@ include 'main.php';
 
             document.getElementById("s_name").value = myObj[0];
             document.getElementById("department").value = myObj[1];
+            document.getElementById("issued_books").value = myObj[2];
+
+            if (myObj[2] === 5 || myObj[2] == -1 || myObj[2] === null) {
+              disableStudent = true;
+              if (myObj[2] === 5) {
+                document.getElementById("issued_books").classList.add("text-white", "bg-danger", "fw-bolder");
+                document.getElementById("issued_books").classList.remove("bg-success");
+              }
+            } else {
+              disableStudent = false;
+              document.getElementById("issued_books").classList.add("text-white", "bg-success", "fw-bolder");
+              document.getElementById("issued_books").classList.remove("bg-danger");
+              console.log(disableStudent);
+              console.log(myObj[2]);
             }
+            if (!disableBook && !disableStudent) {
+              document.getElementById("submit").disabled = false;
+            }
+          }
         };
 
         xmlhttp.open("GET", "getstudentdetails.php?s_id=" + str, true);
